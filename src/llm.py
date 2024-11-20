4c7dd70ac3d2ed5fa82065ae43fc55d727aab427
Jason Zhang <71229867+yilinjz@users.noreply.github.com>
2024-11-20 00:44:07 -0500

    filtered anomaly data to obtain "potential sarcasm" and ran llm classification

--- src/llm.py
+++ src/llm.py
@@ -22,22 +22,25 @@ def main():
     model = ChatOllama(model=LLM_NAME, temperature=0)
 
     # read the CSV file and populate the dictionary
-    anomaly_data = []
     anomaly_data_path = "dataset/yelp_anomaly.csv"
+    sarcasm_data = []
+    sarcasm_data_path = "dataset/yelp_sarcasm.json"
+    Path(sarcasm_data_path).parent.mkdir(parents=True, exist_ok=True)
     with open(anomaly_data_path, 'r', encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file)  # automatically uses the first line as keys
-        with open("dataset/yelp_anomaly.json", 'w', encoding='utf-8') as json_file:
+        with open(sarcasm_data_path, 'w', encoding='utf-8') as json_file:
             for row in reader:
-                anomaly_data.append(row)
-                json.dump(row, json_file)
-                json_file.write('\n')
+                if float(row['anomaly_score']) >= 1/3:
+                    sarcasm_data.append(row)
+                    json.dump(row, json_file)
+                    json_file.write('\n')
 
     # write system outputs
     system_output = []
     output_file = "output/llm_sarcasm_labels.txt"
     Path(output_file).parent.mkdir(parents=True, exist_ok=True)
     # label test outputs for comparison and analysis
-    for sample in tqdm(anomaly_data):
+    for sample in tqdm(sarcasm_data):
         query = sample['text']
         response = get_llm_response(query=query, model=model)
         system_output.append(response)
